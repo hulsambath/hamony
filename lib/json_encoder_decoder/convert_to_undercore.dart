a28552bfc2a01ecd20a0b5a24e2cd8ec414b49aa
hulsambath <hulsambath14@gmail.com>
2025-01-19 22:06:18 +0700

    Close #13 setup github workflow to check code quality when push to repo

--- lib/json_encoder_decoder/convert_to_undercore.dart
+++
@@ -1,9 +0,0 @@
-class JsonConverters {
-  const JsonConverters._();
-
-  static String convertToUnderscore(String input) {
-    return input.replaceAllMapped(RegExp(r'([a-z])([A-Z])'), (match) {
-      return '${match.group(1)}_${match.group(2)?.toLowerCase()}';
-    });
-  }
-}
--- lib/json_encoder_decoder/extensions/is_exactly.dart
+++
@@ -1,7 +0,0 @@
-import 'package:macros/macros.dart';
-
-extension IsExactly on TypeDeclaration {
-  /// Cheaper than checking types using a [StaticType].
-  bool isExactly(String name, Uri library) =>
-      identifier.name == name && this.library.uri == library;
-}
